2d07c1afe0cca9387f67f193aad5ddc56fb44c5b
Danilo Tedeschi <danilof.t2@gmail.com>
2019-11-29 00:05:55 -0300

    Add poly_function

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ main: eig.o e3p.cpp
 
 eig.o: eig.cpp eig.h
 	$(CC) -c eig.cpp -o eig.o $(CFLAGS)
+
+poly_function.o: poly_function.cpp poly_function.h
+	$(CC) -c poly_function.cpp -o poly_function.o $(CFLAGS)
---
+++ poly_function.h
@@ -0,0 +1,19 @@
+#ifndef POLY_FUNCTION_H
+#define POLY_FUNCTION_H
+
+double poly_6_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_6_im(double a, double b, double x1, double x2, double y1, double y2);
+double poly_5_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_5_im(double a, double b, double x1, double x2, double y1, double y2);
+double poly_4_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_4_im(double a, double b, double x1, double x2, double y1, double y2);
+double poly_3_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_3_im(double a, double b, double x1, double x2, double y1, double y2);
+double poly_2_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_2_im(double a, double b, double x1, double x2, double y1, double y2);
+double poly_1_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_1_im(double a, double b, double x1, double x2, double y1, double y2);
+double poly_0_re(double a, double b, double x1, double x2, double y1, double y2);
+double poly_0_im(double a, double b, double x1, double x2, double y1, double y2);
+
+#endif
